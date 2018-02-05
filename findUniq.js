49993d90087d13a8d5884217c43d0fd348d80ad9
nandhini <nandhini31@gmail.com>
2018-02-05 12:12:14 +0000

    fixed edge cases

--- findUniq.js
+++ findUniq.js
@@ -14,5 +14,6 @@ for (var i = 0; i < array.length; i++) {
 
 
 console.log(findUniq([ 0, 0, 0.55, 0, 0 ]))
-//console.log(findUniq([0,0,0,0,0,0,1])
-//findUniq([0,0,0,0,0,1])
+//console.log(findUniq([1,0,0,0,0,0,0])
+//console.log(findUniq([-0.55,0,0,0,0,0,1])
+//console.log(findUniq([1,1,1,1,2,1,1])
--- subsetSum.js
+++ subsetSum.js
@@ -1,5 +1,5 @@
 function combos(num){
-  if(num < 0 || num > 29){
+  if(num <= 0 || num > 29){
     console.log("Please provide a number between 0 and 29")
     return;
   }
@@ -8,23 +8,23 @@ function combos(num){
     let comboArray = []
     recursiveSum(array,0,num,num,comboArray);
     console.log("Array that has all combinations",comboArray)
-  } 
+  }
 }
 
 function recursiveSum(arr=[],index,num,sum,comboArray){
   let subArray = [] ;
   if(sum < 0)
     return ;
-    
+
   if(sum === 0){
     for(var i = 0; i< index;i++)
       subArray.push(arr[i])
-    comboArray.push(subArray)  
+    comboArray.push(subArray)
   return comboArray;
   }
 
   let prevItem = (index === 0)? 1 : arr[index-1];
-  
+
   for(var j = prevItem; j<= num; j++){
     arr[index] = j;
     recursiveSum(arr,index+1,num,sum-j,comboArray)
@@ -32,3 +32,7 @@ function recursiveSum(arr=[],index,num,sum,comboArray){
 }
 
 combos(3)
+// combos(5)
+// combos(29)
+// combos(0)
+// combos(32)
